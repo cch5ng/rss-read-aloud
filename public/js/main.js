cc965acde36d20d75af0c21efd8f86eccc87b8c3
cch5ng <cch5ng@gmail.com>
2015-11-24 19:58:29 -0800

    test: dev test for audio reading for loop iteration too fast and so not all headlines are getting read, only first and last (4 times)

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     var element = document.querySelector('voice-player');
 
-    element.setAttribute('text', element.innerText);
     $('.btnNpr').click( function() {
-	    console.log('text: ' + element.innerText);
-      element.setAttribute('text', element.innerText);
-      element.speak();
+	    var feedNodes = document.getElementsByClassName('feed');
+	    var feedNodesLength = feedNodes.length;
+	    console.log('feedNodes length: ' + feedNodes.length);
+	    //console.log('text: ' + element.innerText);
+		for (var i = 0; i < feedNodesLength; i++) {
+			//window.setTimeout(function() {
+				var toRead = feedNodes[i].querySelector('h3').innerText;
+				console.log('text: ' + toRead);
+				//text += feedNodes[i].querySelector('p').innerHTML;
+				//console.log('text: ' + text);
+				element.setAttribute('text', toRead);
+				element.speak();
+			//}, 3000);
+		}
     });
 })
 
