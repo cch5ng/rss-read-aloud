bd765d6cd41444c915dd2d5dfdc76c92b2794774
cch5ng <cch5ng@gmail.com>
2015-12-02 11:42:50 -0800

    feat: update click listeners to handle events for fa audio control icons

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
     var feed;
 	var feedNodes;
     var feedNodesLength;
+    var audioStates = ['playing', 'stopped', 'paused'];
+    //audioState can be 
+    var audioState = audioStates[1];
 
     function playNextFeed() {
 		feed = feedNodes[counter].querySelector('h3').innerText;
@@ -15,21 +18,50 @@ document.addEventListener('DOMContentLoaded', function() {
 		element.speak();
     }
 
-//TODO abstract this to play appropriate feeds per button
-//TODO figure out if there is some way to create one global button that would play all feeds?
-    $('.btnNpr').click( function() {
-    	feedNodes = document.getElementsByClassName('feed');
-    	feedNodesLength = feedNodes.length;
-	    console.log('feedNodes length: ' + feedNodes.length);
-	    playNextFeed();
+//TODO add handling for the audio resume function
+//if condition for if state is 'stopped' (then play from beginning) vs 'paused' (then resume)
+    $('.fa-play').click( function() {
+        //if audio stopped
+        if (audioState === audioStates[1]) {
+            feedNodes = document.getElementsByClassName('feed');
+            feedNodesLength = feedNodes.length;
+            console.log('feedNodes length: ' + feedNodes.length);
+            playNextFeed();
+        } else if (audioState === audioStates[2]) {
+        //if audio paused
+            element.resume();
+        }
+        audioState = audioStates[0];
     });
 
-    $('.btnYahoo').click( function() {
-    	feedNodes = document.getElementsByClassName('yahoo-feed');
-    	feedNodesLength = feedNodes.length;
-	    console.log('feedNodes length: ' + feedNodes.length);
-	    playNextFeed();
+//TODO click listener for audio Pause button
+    $('.fa-pause').click( function() {
+        element.pause();
+        audioState = audioStates[2];
     });
+
+//TODO click listener for audio Stop button (audio cancel)
+    $('.fa-stop').click( function() {
+        element.cancel();
+        element.setAttribute('text', '');
+        audioState = audioStates[1];
+    });
+
+//TODO abstract this to play appropriate feeds per button
+//TODO figure out if there is some way to create one global button that would play all feeds?
+    // $('.btnNpr').click( function() {
+    // 	feedNodes = document.getElementsByClassName('feed');
+    // 	feedNodesLength = feedNodes.length;
+	   //  console.log('feedNodes length: ' + feedNodes.length);
+	   //  playNextFeed();
+    // });
+
+    // $('.btnYahoo').click( function() {
+    // 	feedNodes = document.getElementsByClassName('yahoo-feed');
+    // 	feedNodesLength = feedNodes.length;
+	   //  console.log('feedNodes length: ' + feedNodes.length);
+	   //  playNextFeed();
+    // });
 
     element.addEventListener('end', function() {
     	console.log('gets here');
